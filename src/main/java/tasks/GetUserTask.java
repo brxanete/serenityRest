f7c3d822fdfc427b3850f2983a56f6238b742b24
BryanWindows <brxanete@outlook.com>
2024-08-29 17:55:35 -0500

    First Commit

---
+++ src/main/java/tasks/GetUserTask.java
@@ -0,0 +1,30 @@
+package tasks;
+
+import io.restassured.http.ContentType;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.rest.interactions.Get;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+public class GetUserTask implements Task {
+    private final int page;
+
+    public GetUserTask(int page) {
+        this.page = page;
+    }
+
+    public static Performable fromPage(int page) {
+        return instrumented(GetUserTask.class, page);
+    }
+
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Get.resource("/users?page=2")
+                        .with(requestSpecification -> requestSpecification.contentType(ContentType.JSON).header("header1", "value1")
+                        ));
+    }
+}
